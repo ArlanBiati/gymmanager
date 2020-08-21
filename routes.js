98897f5302b10ea966422e2960ab8852bdf75298
Arlan <arlangustavo@hotmail.com>
2020-08-20 21:07:06 -0300

    Iniciando Aplicacao: page Instrutores

---
+++ routes.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const routes = express.Router()
+
+
+
+
+routes.get("/", function(req, res) {
+    return res.redirect("instructors")
+})
+
+routes.get("/instructors", function(req, res) {
+    return res.render("instructors/instructors")
+})
+
+routes.get("/members", function(req, res) {
+    return res.render("members")
+})
+
+
+
+
+module.exports = routes
